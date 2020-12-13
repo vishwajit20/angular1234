abc26b38fedd6541931ab3f0196f31c732b42b7b
vishwajit20 <jeet.khade123@gmail.com>
2020-12-13 21:40:10 +0530

    the

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,16 @@
 
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB3SYEDsPucWuw_qKMJgXyz2Qsq87tZjSA",
+    authDomain: "angular-bd5b0.firebaseapp.com",
+    databaseURL: "https://angular-bd5b0-default-rtdb.firebaseio.com",
+    projectId: "angular-bd5b0",
+    storageBucket: "angular-bd5b0.appspot.com",
+    messagingSenderId: "272757872352",
+    appId: "1:272757872352:web:4e105aa84e7cebf9fc2285",
+    measurementId: "G-204BE8YVPJ"
+  }
 };
 
